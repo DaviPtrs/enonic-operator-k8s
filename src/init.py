20959cf94b0811d8ed68cc5da43f7ec9b3cc840f
Davi Petris <davispetris@gmail.com>
2020-10-27 09:40:55 -0300

    code cleaning and black lint

--- src/init.py
+++ src/init.py
@@ -1,11 +1,12 @@
-import asyncio
-import kopf
 import time
+
+import kopf
 import pykube as pk
 
-tasks = {}
 
-@kopf.on.create('apps', 'v1', 'statefulsets', annotations={'enonic-operator-managed': kopf.PRESENT})
+@kopf.on.create(
+    "apps", "v1", "statefulsets", annotations={"enonic-operator-managed": kopf.PRESENT}
+)
 def init_fn(name, namespace, **kwargs):
     api = pk.HTTPClient(pk.KubeConfig.from_env())
     while True:
@@ -14,53 +15,54 @@ def init_fn(name, namespace, **kwargs):
             break
         except pk.exceptions.ObjectDoesNotExist:
             pass
+
+    spec = statefulset.obj["spec"]["template"]["spec"]
+
+    exit_volume = {"name": "exit-folder", "emptyDir": {}}
+    spec["volumes"].append(exit_volume)
+
+    sidecar_container = {
+        "name": "enonic-sidecar",
+        "image": "daviptrs/enonic-operator-k8s-sidecar:alpha",
+        "imagePullPolicy": "Always",
+        "env": [
+            {"name": "DEBUG", "value": "False"},
+            {
+                "name": "ENONIC_AUTH",
+                "valueFrom": {"secretKeyRef": {"name": f"{name}-auth", "key": "auth"}},
+            },
+        ],
+        "volumeMounts": [{"name": "exit-folder", "mountPath": "/exit"}],
+    }
+    spec["containers"].append(sidecar_container)
+
+    xp_container = spec["containers"][0]
     
-    exit_volume = {'name': 'exit-folder', 'emptyDir': {}}
-    (statefulset.obj["spec"]["template"]["spec"]["volumes"]).append(exit_volume)
+    xp_pre_stop = {
+        "exec": {
+            "command": [
+                "/bin/bash",
+                "-c",
+                'while [ -z "$(ls /exit)" ];do sleep 1; done',
+            ]
+        }
+    }
 
-    sidecar_container = {'name': 'enonic-sidecar', 'image': 'daviptrs/enonic-operator-k8s-sidecar:alpha', 'imagePullPolicy': 'Always','env': [{'name': 'ENONIC_AUTH', 'valueFrom': {'secretKeyRef': {'name': f'{name}-auth', 'key': 'auth'}}}], 'volumeMounts': [{'name': 'exit-folder','mountPath': '/exit'}]}
-    (statefulset.obj['spec']['template']['spec']['containers']).append(sidecar_container)
+    if xp_container.get("lifecycle") is None:
+        xp_container["lifecycle"] = {}
+    xp_container["lifecycle"]["preStop"] = xp_pre_stop
 
-    xp_pre_stop = {'exec': {'command': ['/bin/bash', '-c', 'while [ -z "$(ls /exit)" ];do sleep 1; done']}}
-    if statefulset.obj['spec']['template']['spec']['containers'][0].get('lifecycle') is None:
-        statefulset.obj['spec']['template']['spec']['containers'][0]['lifecycle'] = {}
-    statefulset.obj['spec']['template']['spec']['containers'][0]['lifecycle']['preStop'] = xp_pre_stop
+    if xp_container.get("livenessProbe") is None:
+        xp_liveness = {
+            "httpGet": {"path": "/cluster.elasticsearch", "port": 2609},
+            "failureThreshold": 2,
+            "initialDelaySeconds": 30,
+            "periodSeconds": 10,
+        }
+        xp_container["livenessProbe"] = xp_liveness
 
-    if statefulset.obj['spec']['template']['spec']['containers'][0].get('livenessProbe') is None:
-        xp_liveness = {'httpGet': {'path': '/cluster.elasticsearch', 'port': 2609}, 'failureThreshold': 2, 'initialDelaySeconds': 30, 'periodSeconds': 10}
-        statefulset.obj['spec']['template']['spec']['containers'][0]['livenessProbe'] = xp_liveness
-
-    xp_exit_volume = {'name': 'exit-folder', 'mountPath': '/exit'}
-    (statefulset.obj['spec']['template']['spec']['containers'][0]['volumeMounts']).append(xp_exit_volume)
-
+    xp_exit_volume = {"name": "exit-folder", "mountPath": "/exit"}
+    xp_container["volumeMounts"].append(xp_exit_volume)
 
     statefulset.update()
     api.session.close()
-
-
-# @kopf.on.event('', 'v1', 'pods', annotations={'enonic-operator-managed': kopf.PRESENT})
-# def init_fn(event, status, **kwargs):
-#     ignored_conditions = [
-#         event is None, 
-#         status is None,
-#         event is '',
-#         status is '',
-#         event.get('type') == None,
-#         status.get('phase') != "Running",
-#         event['object']['metadata'].get('deletionGracePeriodSeconds') != None 
-#     ]
-#     if any(ignored_conditions):
-#         return
-    
-#     api = pk.HTTPClient(pk.KubeConfig.from_env())
-
-#     api.session.close()
-    
-
-
-# @kopf.on.delete('', 'v1', 'pods', annotations={'enonic-operator-managed': kopf.PRESENT})
-# def delete_fn(status, **kwargs):
-#     print("\n\n\n\n\n================\n\n\n\n")
-
-#     time.sleep(60)
-#     print(status['podIP'])
