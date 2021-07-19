e3c2094c06edbe4cab1f058b355b0c0298941959
Davi Petris <davispetris@gmail.com>
2021-07-19 15:40:17 -0300

    add health check

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
+#TODO: Create a var to store common arguments
 INITPATH=$PWD/init.py
 
 if [[ DEBUG -eq 1 ]]; then
-    kopf run -A "$INITPATH" --verbose
+    kopf run --liveness=http://0.0.0.0:8080/healthz -A "$INITPATH" --verbose
     # echo "debug"
 elif [[ QUIET -eq 1 ]]; then
-    kopf run -A "$INITPATH" --quiet
+    kopf run --liveness=http://0.0.0.0:8080/healthz -A "$INITPATH" --quiet
     # echo "quiet"
 else
     # echo "normal"
-    kopf run -A "$INITPATH" 
+    kopf run --liveness=http://0.0.0.0:8080/healthz -A "$INITPATH"
 fi
